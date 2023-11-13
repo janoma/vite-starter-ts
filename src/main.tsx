9737cba333fe9f2586b355959010f8900464941b
Alejandro Mallea <janoma@gmail.com>
2023-11-12 21:30:03 -0800

    remove unneeded files and boilerplate code

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import { CssVarsProvider } from "@mui/joy/styles";
+import CssBaseline from "@mui/joy/CssBaseline";
+
+import App from "./App.tsx";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <CssVarsProvider>
+      <CssBaseline />
+      <App />
+    </CssVarsProvider>
   </React.StrictMode>,
 );
